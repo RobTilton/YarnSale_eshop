f5f2f08dc494356975015554981839a0651a3346
Robert T <RobertJTilton@gmail.com>
2024-01-12 12:25:27 -0500

    new app, frontend focused, had to change path to project level.

--- YarnSale/settings.py
+++ YarnSale/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
     # Custom Apps
     'shop',
+    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -61,7 +63,7 @@ ROOT_URLCONF = 'YarnSale.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'YarnSale', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
