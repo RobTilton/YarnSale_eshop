57d6c6da903f8f5b55c9d9a3d29779363585d6f1
Robert T <RobertJTilton@gmail.com>
2024-01-15 19:50:13 -0500

    connecting placeholders to navbar links.

--- YarnSale/urls.py
+++ YarnSale/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('shop.urls')),
-    path('frontend/', include('frontend.urls')),
+    path('', include('frontend.urls')),
     
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
 ]
