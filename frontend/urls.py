84d04243c889246c6638b7ccd3582ac58ee5387d
Robert T <RobertJTilton@gmail.com>
2024-01-19 19:31:56 -0500

    fixed that issue on server start.

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
     path('shop/', views.shop, name='shop'),
